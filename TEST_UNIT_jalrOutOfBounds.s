07a89a44c9876913bc4e57e91365f016dd09c0d1
Cameron Cheung <cameroncheung@berkeley.edu>
2019-11-10 00:43:00 -0800

    A

--- TEST_UNIT_jalrOutOfBounds.s
+++ TEST_UNIT_jalrOutOfBounds.s
@@ -1,5 +1,5 @@
 jal x0 12
 jalr ra ra 8
 add x0 x0 x0
-addi ra ra 4
+addi ra x0 4
 jalr ra ra 0
---
+++ my_tests/input/TEST_UNIT_jalrInBounds.s
@@ -0,0 +1,8 @@
+jal x0 12
+jalr ra ra 8
+add x0 x0 x0
+addi ra ra 4
+jalr ra ra 0
+add x0 x0 x0
+add x0 x0 x0
+add x0 x0 x0
