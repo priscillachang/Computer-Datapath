ee02977e4d6988f387a4ae1bf841381ccfb5f43a
Cameron Cheung <cameroncheung@berkeley.edu>
2019-11-17 09:34:45 -0800

    O

--- TEST_UNIT_add.s
+++ TEST_UNIT_add.s
@@ -1,8 +1,85 @@
-add sp x0 x0 
-addi ra x0 2
-addi s1 x0 7
-add s0 ra s1
-add s0 s1 ra
-add sp sp sp
-addi s1 s1 -5
-add t0 ra s1
+add t0 x0 x0
+addi t1 x0 1
+
+add t2 t1 x0
+add t1 t1 t0
+add t0 t2 x0
+
+add t2 t1 x0
+add t1 t1 t0
+add t0 t2 x0
+
+add t2 t1 x0
+add t1 t1 t0
+add t0 t2 x0
+
+add t2 t1 x0
+add t1 t1 t0
+add t0 t2 x0
+
+add t2 t1 x0
+add t1 t1 t0
+add t0 t2 x0
+
+add t2 t1 x0
+add t1 t1 t0
+add t0 t2 x0
+
+add t2 t1 x0
+add t1 t1 t0
+add t0 t2 x0
+
+add t2 t1 x0
+add t1 t1 t0
+add t0 t2 x0
+
+add t2 t1 x0
+add t1 t1 t0
+add t0 t2 x0
+
+add t2 t1 x0
+add t1 t1 t0
+add t0 t2 x0
+
+add t0 x0 x0
+addi t1 x0 -1
+
+add t2 t1 x0
+add t1 t1 t0
+add t0 t2 x0
+
+add t2 t1 x0
+add t1 t1 t0
+add t0 t2 x0
+
+add t2 t1 x0
+add t1 t1 t0
+add t0 t2 x0
+
+add t2 t1 x0
+add t1 t1 t0
+add t0 t2 x0
+
+add t2 t1 x0
+add t1 t1 t0
+add t0 t2 x0
+
+add t2 t1 x0
+add t1 t1 t0
+add t0 t2 x0
+
+add t2 t1 x0
+add t1 t1 t0
+add t0 t2 x0
+
+add t2 t1 x0
+add t1 t1 t0
+add t0 t2 x0
+
+add t2 t1 x0
+add t1 t1 t0
+add t0 t2 x0
+
+add t2 t1 x0
+add t1 t1 t0
+add t0 t2 x0
