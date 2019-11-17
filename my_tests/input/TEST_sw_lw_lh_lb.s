851416fad338da13d02c77ff7f190f30c0608433
Cameron Cheung <cameroncheung@berkeley.edu>
2019-11-17 10:02:07 -0800

    A

--- my_tests/input/TEST_sw_lw_lh_lb.s
+++ my_tests/input/TEST_sw_lw_lh_lb.s
@@ -1,31 +1,26 @@
-addi sp sp -4
-sw x0 0(sp)
-lw x0 0(sp)
-lh x0 0(sp)
-lb x0 0(sp)
-#lui a0 1043916
-#addi a0 a0 -1384
+lui a0 1043916
+addi a0 a0 -1384
 
-#lui t2 74565
-#addi t2 t2 1656
+lui t2 74565
+addi t2 t2 1656
 
-#addi sp sp -8
-#sw a0 0(sp)
-#sw t2 4(sp)
+addi sp sp -8
+sw a0 0(sp)
+sw t2 4(sp)
 
-#lb t0 0(sp)
-#lb s0 1(sp)
-#lb s1 2(sp)
-#lb s1 3(sp)
-#lh s0 0(sp)
-#lh t0 2(sp)
-#lw s0 0(sp)
+lb t0 0(sp)
+lb s0 1(sp)
+lb s1 2(sp)
+lb s1 3(sp)
+lh s0 0(sp)
+lh t0 2(sp)
+lw s0 0(sp)
 
-#lb t0 4(sp)
-#lb s1 5(sp)
-#lb s0 6(sp)
-#lb t0 7(sp)
-#lh s1 4(sp)
-#lh s0 6(sp)
-#lw t0 4(sp)
-#add a0 x0 x0
+lb t0 4(sp)
+lb s1 5(sp)
+lb s0 6(sp)
+lb t0 7(sp)
+lh s1 4(sp)
+lh s0 6(sp)
+lw t0 4(sp)
+add a0 x0 x0
