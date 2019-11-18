156de5103cb63fd499e50d9300e313d12f4d86ab
Cameron Cheung <cameroncheung@berkeley.edu>
2019-11-18 02:01:26 -0800

    A

--- TEST_UNIT_lui.s
+++ TEST_UNIT_lui.s
@@ -1,14 +1,14 @@
-lui x0 67
-lui ra 55555
-lui sp 22299
-lui t0 1
-addi t1 x0 999
-#lui t1 10234
-#lui t2 999999
-#addi s0 s0 -1
-#lui s0 456
-#lui s1 0
-#addi s1 s1 999
+#lui x0 67
+#lui ra 55555
+#lui sp 22299
+#lui t0 1
+#addi t1 x0 999
+lui t1 10234
+lui t2 999999
+addi s0 s0 -1
+lui s0 456
+lui s1 0
+addi s1 s1 999
 #lui a0 1048575
 #addi a0 a0 2047
 #lui a0 581201
