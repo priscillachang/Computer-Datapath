f6c27c6e02001a10b1cbc1199b5093ddfc9f74f0
Cameron Cheung <cameroncheung@berkeley.edu>
2019-11-18 01:23:06 -0800

    A

--- TEST_EDGE.s
+++ TEST_EDGE.s
@@ -1,5 +1,25 @@
-lui t0 524288
-addi t0 t0 -1
-jal ra label
-label: add x0 x0 x0
-jalr t0 t0 0
+lui t1 920371
+addi t1 t1 1255
+
+addi t0 x0 2047
+addi t0 x0 2048
+addi t0 x0 4095
+
+slti t0 t0 2047
+slti t0 t1 2048
+slti t0 t1 4095
+
+xori t0 t0 2047
+xori t0 t1 2048
+xori t0 t1 4095
+
+ori t0 t0 2047
+ori t0 t1 2048
+ori t0 t1 4095
+
+andi t0 t0 2047
+andi t0 t1 2048
+andi t0 t1 4095
+
+addi sp sp -1
+sw t1 -80(sp)
