b9519c221a3fc4eefc39934a52e284b2b23f86d1
Cameron Cheung <cameroncheung@berkeley.edu>
2019-11-18 01:27:07 -0800

    A

--- TEST_EDGE.s
+++ TEST_EDGE.s
@@ -3,23 +3,18 @@ addi t1 t1 1255
 
 addi t0 x0 2047
 addi t0 x0 2048
-addi t0 x0 4095
 
 slti t0 t0 2047
 slti t0 t1 2048
-slti t0 t1 4095
 
 xori t0 t0 2047
 xori t0 t1 2048
-xori t0 t1 4095
 
 ori t0 t0 2047
 ori t0 t1 2048
-ori t0 t1 4095
 
 andi t0 t0 2047
 andi t0 t1 2048
-andi t0 t1 4095
 
-addi sp sp -1
+addi sp sp -8
 sw t1 -80(sp)
