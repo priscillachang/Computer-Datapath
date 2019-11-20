f200b1c8d3f146c4e7da8cc1359c49cd65c9c26e
Cameron Cheung <cameroncheung@berkeley.edu>
2019-11-19 16:24:49 -0800

    A

--- my_tests/input/TEST_EDGE.s
+++ my_tests/input/TEST_EDGE.s
@@ -1,6 +1,33 @@
-lui t1 524288
-addi t1 t1 -4
-addi t0 t0 1200
-jalr t1 t0 0
-add x0 x0 x0
-add x0 x0 x0
+lui a0 1043916
+addi a0 a0 -1384
+
+lui t2 74565
+addi t2 t2 1656
+
+addi sp sp -8
+sw a0 0(sp)
+sw t2 4(sp)
+
+addi t0 x0 -1
+lb t0 0(sp)
+addi s0 x0 -1
+lb s0 1(sp)
+addi s1 x0 -1
+lb s1 2(sp)
+addi s1 x0 -1
+lb s1 3(sp)
+addi s0 x0 -1
+lh s0 0(sp)
+addi t0 x0 -1
+lh t0 2(sp)
+addi s0 x0 -1
+lw s0 0(sp)
+
+lb t0 4(sp)
+lb s1 5(sp)
+lb s0 6(sp)
+lb t0 7(sp)
+lh s1 4(sp)
+lh s0 6(sp)
+lw t0 4(sp)
+add a0 x0 x0
--- my_tests/input/TEST_UNIT_slli.s
+++ my_tests/input/TEST_UNIT_slli.s
@@ -27,3 +27,165 @@ slli t2 t2 31
 addi t0 x0 -32
 slli t0 t0 2
 slli t0 t0 0
+
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 0
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 1
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 2
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 3
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 4
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 5
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 6
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 7
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 8
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 9
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 10
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 11
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 12
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 13
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 14
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 15
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 16
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 17
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 18
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 19
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 20
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 21
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 22
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 23
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 24
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 25
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 26
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 27
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 28
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 29
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 30
+lui t0 74565
+addi t0 t0 1656
+slli t0 t0 31
+
+addi t0 t0 -1
+slli t0 t0 0
+addi t0 t0 -1
+slli t0 t0 1
+addi t0 t0 -1
+slli t0 t0 2
+addi t0 t0 -1
+slli t0 t0 3
+addi t0 t0 -1
+slli t0 t0 4
+addi t0 t0 -1
+slli t0 t0 5
+addi t0 t0 -1
+slli t0 t0 6
+addi t0 t0 -1
+slli t0 t0 7
+addi t0 t0 -1
+slli t0 t0 8
+addi t0 t0 -1
+slli t0 t0 9
+addi t0 t0 -1
+slli t0 t0 10
+addi t0 t0 -1
+slli t0 t0 11
+addi t0 t0 -1
+slli t0 t0 12
+addi t0 t0 -1
+slli t0 t0 13
+addi t0 t0 -1
+slli t0 t0 14
+addi t0 t0 -1
+slli t0 t0 15
+addi t0 t0 -1
+slli t0 t0 16
+addi t0 t0 -1
+slli t0 t0 17
+addi t0 t0 -1
+slli t0 t0 18
+addi t0 t0 -1
+slli t0 t0 19
+addi t0 t0 -1
+slli t0 t0 20
+addi t0 t0 -1
+slli t0 t0 21
+addi t0 t0 -1
+slli t0 t0 22
+addi t0 t0 -1
+slli t0 t0 23
+addi t0 t0 -1
+slli t0 t0 24
+addi t0 t0 -1
+slli t0 t0 25
+addi t0 t0 -1
+slli t0 t0 26
+addi t0 t0 -1
+slli t0 t0 27
+addi t0 t0 -1
+slli t0 t0 28
+addi t0 t0 -1
+slli t0 t0 29
+addi t0 t0 -1
+slli t0 t0 30
+addi t0 t0 -1
+slli t0 t0 31
